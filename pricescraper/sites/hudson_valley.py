8d68152345f1025e69dbc3464745e8e80b77784d
elohwhydee@gmail.com <jnuhhbgv1!>
2017-09-03 00:14:00 -0400

    The search URL for Seed Savers has changed

--- pricescraper/sites/hudson_valley.py
+++ pricescraper/sites/hudson_valley.py
@@ -9,14 +9,14 @@ class HudsonValley(BaseSite):
     '''This class scrapes Product data from SeedLibrary.org'''
     ABBREVIATION = 'hv'
 
-    ROOT_URL = 'http://www.seedlibrary.org'
+    ROOT_URL = 'http://hudsonvalleyseed.com'
     SEARCH_URL = ROOT_URL + '/catalogsearch/result/?q={}'
     NO_RESULT_TEXT = 'Your search returns no results.'
 
     def _get_results_from_search_page(self, search_page_html):
         '''Return tuples of names & URLs of search results.'''
         return re.findall(
-            r'product-name"><a href=".*?org(.*?)".*?title="(.*?)".*?>',
+            r'product-name"><a href=".*?com(.*?)".*?title="(.*?)".*?>',
             search_page_html)
 
     def _parse_name_from_product_page(self):
@@ -26,7 +26,7 @@ class HudsonValley(BaseSite):
         :rtype: :obj:`str`
 
         '''
-        return self._get_match_from_product_page(r'<title>(.*?)</title>')
+        return self._get_match_from_product_page(r'<title>\s*(.*?)<\/title>')
 
     def _parse_number_from_product_page(self):
         '''Parse the Product's Number from the Product Page.
@@ -35,7 +35,7 @@ class HudsonValley(BaseSite):
         :rtype: :obj:`str`
 
         '''
-        return 'None Specified'
+        return self._get_match_from_product_page(r'Product ID# (\S*)')
 
     def _parse_organic_status_from_product_page(self):
         '''Parse the Product's Organic Status from the Product Page.
@@ -44,7 +44,7 @@ class HudsonValley(BaseSite):
         :rtype: :obj:`bool`
 
         '''
-        return 'Certified Organic Seed' in self.page_html
+        return 'Certified Organic' in self.page_html
 
     def _parse_price_from_product_page(self):
         '''Parse the Product's Price from the Product Page.
@@ -62,4 +62,4 @@ class HudsonValley(BaseSite):
         :rtype: :obj:`str`
 
         '''
-        return self._get_match_from_product_page(r'class="data">(.*?)<')
+        return 'Not Specified'
