be10295b0d2a2841c9be25b9c46271cbb70049ce
elohwhydee@gmail.com <jnuhhbgv1!>
2017-09-06 03:37:47 -0400

    changes for fedco

--- pricescraper/sites/fedco_seeds.py
+++ pricescraper/sites/fedco_seeds.py
@@ -3,7 +3,8 @@
 import re
 
 from .base import BaseSite
-from util import remove_punctuation
+from util import remove_punctuation, get_page_html
+
 
 
 class FedcoSeeds(BaseSite):
@@ -13,6 +14,14 @@ class FedcoSeeds(BaseSite):
     ROOT_URL = 'http://www.fedcoseeds.com'
     SEARCH_URL = ROOT_URL + '/seeds/search?search={}'
     SEARCH_REDIRECTED_TEXT = 'Back to Search Results'
+
+    def _search_site(self, search_terms):
+        '''Fedco wants pluses in between serach terms
+        '''
+        assert self.SEARCH_URL is not None
+        escaped_keywords = search_terms.replace(' ', '+')
+        search_url = self.SEARCH_URL.format(escaped_keywords)
+        return get_page_html(search_url)
 
     def _get_results_from_search_page(self, search_page_html):
         '''Return tuples of names & URLs of search results.'''
@@ -27,7 +36,7 @@ class FedcoSeeds(BaseSite):
         :rtype: :obj:`str`
 
         '''
-        return self._get_match_from_product_page(r'Item: (.*?)</title>')
+        return self._get_match_from_product_page(r'Fedco Seeds - (.*?)<\/title>')
 
     def _parse_number_from_product_page(self):
         '''Parse the Product's Number from the Product Page HTML
@@ -37,7 +46,7 @@ class FedcoSeeds(BaseSite):
 
         '''
         return self._get_match_from_product_page(
-            r'<div align="justify">\s+<strong>\s+(.*?)\s.*?<')
+            r'"pricecell">\s(.*?):')
 
     def _parse_organic_status_from_product_page(self):
         '''Parse the Product's Organic Status from the Product Page HTM
@@ -56,7 +65,7 @@ class FedcoSeeds(BaseSite):
         :rtype: :obj:`str`
 
         '''
-        return self._get_match_from_product_page(r'A=\S+ for (\$\d*\.?\d*)')
+        return self._get_match_from_product_page(r'(\$[0-9]*\.[0-9][0-9])')
 
     def _parse_weight_from_product_page(self):
         '''Parse the Product's Weight from the Product Page HTM
@@ -65,4 +74,4 @@ class FedcoSeeds(BaseSite):
         :rtype: :obj:`str`
 
         '''
-        return self._get_match_from_product_page(r'A=(\S+).*?<')
+        return self._get_match_from_product_page(r'"pricecell">\s.*? (.*?) ')
